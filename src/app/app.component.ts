9613e41ecf271b94ef761edd5d92008ce84fd1cf
prmorais13 <prmorais_13@hotmail.com>
2017-08-15 01:08:25 -0300

    Capitulo 9 - Aulas 9.1 a 9.16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nome = 'Paulo Roberto';
+
+  adicionar(nome: string) {
+    this.nome = nome;
+  }
 }
