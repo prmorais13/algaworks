5e5203eb91b6c8d7005229ae5b524683be4a7eef
prmorais13 <prmorais_13@hotmail.com>
2017-08-22 00:19:00 -0300

    Aulas 11.8 a 12.7

--- algamoney-ui/src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ algamoney-ui/src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,19 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentosPesquisaComponent {
 
   lancamentos = [
-    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '17/08/2017', dataPagamento: null,
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 8, 19), dataPagamento: null,
       valor: 4.65, pessoa: 'Padaria Parque Verde'},
-    { tipo: 'DESPESA', descricao: 'Compra de frutas', dataVencimento: '17/08/2017', dataPagamento: null,
+    { tipo: 'DESPESA', descricao: 'Compra de frutas', dataVencimento: new Date(2017, 8, 20), dataPagamento: null,
       valor: 8.96, pessoa: 'Mercadinho Parque Verde'},
-    { tipo: 'RECEITA', descricao: 'Manutenção micro', dataVencimento: '13/08/2017', dataPagamento: '17/08/2017',
-      valor: 150.00, pessoa: 'Aurino Villa'},
-    { tipo: 'DESPESA', descricao: 'Gasolina', dataVencimento: '17/08/2017', dataPagamento: '17/08/2017',
-      valor: 100.00, pessoa: 'Posto Arco Verde'},
-    { tipo: 'RECEITA', descricao: 'Salário Prefeitura', dataVencimento: '30/07/2017', dataPagamento: '11/08/2017',
-      valor: 4773.12, pessoa: 'Prefeitura de Natal'},
-    { tipo: 'DESPESA', descricao: 'Prestação casa', dataVencimento: '20/08/2017', dataPagamento: null,
+    { tipo: 'RECEITA', descricao: 'Manutenção micro', dataVencimento: new Date(2017, 8, 20),
+      dataPagamento: new Date(2017, 8, 21), valor: 150.00, pessoa: 'Aurino Villa'},
+    { tipo: 'DESPESA', descricao: 'Gasolina', dataVencimento: new Date(2017, 8, 19),
+      dataPagamento: new Date(2017, 8, 21), valor: 100.00, pessoa: 'Posto Arco Verde'},
+    { tipo: 'RECEITA', descricao: 'Salário Prefeitura', dataVencimento: new Date(2017, 8, 18),
+      dataPagamento: new Date(2017, 8, 20), valor: 4773.12, pessoa: 'Prefeitura de Natal'},
+    { tipo: 'DESPESA', descricao: 'Prestação casa', dataVencimento: new Date(2017, 8, 20), dataPagamento: null,
       valor: 602.25, pessoa: 'Caixa Econômica'},
-    { tipo: 'DESPESA', descricao: 'Energia elétrica', dataVencimento: '26/08/2017', dataPagamento: null,
+    { tipo: 'DESPESA', descricao: 'Energia elétrica', dataVencimento: new Date(2017, 8, 26), dataPagamento: null,
       valor: 55.82, pessoa: 'COSERN'}
   ]
 
