06abfcf62cc847e11b4b3cf321a0b86b79aef755
prmorais13 <prmorais_13@hotmail.com>
2017-08-26 01:06:06 -0300

    Alteração teste

--- servicos/src/app/funcionario-card/funcionario-card.component.ts
+++ servicos/src/app/funcionario-card/funcionario-card.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-funcionario-card',
   templateUrl: './funcionario-card.component.html',
-  // styleUrls: ['./funcionario-card.component.css']
   styles: [`
     .card-block {
       text-transform: uppercase;
