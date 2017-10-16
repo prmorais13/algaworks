1a398a249a0d168a4eacffe091df612be7410843
prmorais13 <prmorais_13@hotmail.com>
2017-10-16 19:06:39 -0300

    Ajustes

--- algamoney-ui/src/environments/environment.prod.ts
+++ algamoney-ui/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // apiUrl: 'http://localhost:8080'
   // apiUrl: 'http://prmorais.ddns.net:8080'
-  apiUrl: 'https://api-curso.herokuapp.com'
+  apiUrl: 'https://curso-api.herokuapp.com'
 };
